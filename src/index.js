97f79086f7b3af6e2c42dd732d484c4f6743852f
nomad8t <serega18t@tut.by>
2020-10-16 17:21:34 +0300

    feat: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const CFG = bracketsConfig.map((el) => el.join``);
+  const CFG_LEN = CFG.length;
+
+  for (let i = 0; i < CFG_LEN; i++) {
+    while (str.includes(CFG[i])) {
+      str = str.replace(CFG[i], '');
+      i -= CFG_LEN;
+    }
+  }
+
+  return str === '';
 }
